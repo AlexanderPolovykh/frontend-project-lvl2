a638b2cac99feb0709db9c6862130972bcd1f3da
polovykh <polovykh@mail.ru>
2022-06-10 12:40:30 +0700

    Вычислитель отличий: Step 5+

--- src/code.js
+++ src/code.js
@@ -8,33 +8,34 @@ export default (file1, file2) => {
   const obj2 = parseFile(file2);
   const entries1 = Object.entries(obj1);
   const entries2 = Object.entries(obj2);
-  const generalEntries = _.intersectionWith(entries1, entries2, _.isEqual);
+  const generalEntries = _
+    .intersectionWith(entries1, entries2, _.isEqual)
+    .map((el) => [...el, 0]);
   const unionEntries = _.unionWith(entries1, entries2, _.isEqual);
-  const sortedUnionEntries = _.sortBy(unionEntries, (el) => el[0]);
+  const diffEntries1 = _
+    .xorWith(unionEntries, entries2, _.isEqual)
+    .map((el) => [...el, -1]);
+  const diffEntries2 = _
+    .xorWith(unionEntries, entries1, _.isEqual)
+    .map((el) => [...el, 1]);
+  const allEntries = [...generalEntries, ...diffEntries1, ...diffEntries2];
+  const sortedUnionEntries = _.sortBy(allEntries, (el) => el[0]);
   const arrStr = ["{"];
-  sortedUnionEntries.forEach(([key, value]) => {
-    let dontExit = true;
-    generalEntries.forEach((el) => {
-      if (_.isEqual(el, [key, value])) {
-        arrStr.push(`    ${key}: ${value}`);
-        dontExit = false;
-      }
-    });
-    if (dontExit) {
-      entries1.forEach((el) => {
-        if (_.isEqual(el, [key, value])) {
-          arrStr.push(`  - ${key}: ${value}`);
-          dontExit = false;
-        }
-      });
+  sortedUnionEntries.forEach(([key, value, flag]) => {
+    let ch = " ";
+    switch (flag) {
+      case 0:
+        ch = " ";
+        break;
+      case -1:
+        ch = "-";
+        break;
+      case 1:
+        ch = "+";
+        break;
+      default:
     }
-    if (dontExit) {
-      entries2.forEach((el) => {
-        if (_.isEqual(el, [key, value])) {
-          arrStr.push(`  + ${key}: ${value}`);
-        }
-      });
-    }
+    arrStr.push(`  ${ch} ${key}: ${value}`);
   });
   arrStr.push(["}"]);
   retStr = arrStr.join("\n");
